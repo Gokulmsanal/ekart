4771fffebfb47fffc8850c14181d2795b2ea4bfb
Gokulmsanal <gokulmsanal23@gmail.com>
2024-01-11 22:02:54 +0530

    second commit

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+
+from django.contrib.auth.models import User
+
 import datetime
 import os
 
@@ -46,3 +49,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_qty = models.IntegerField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -14,7 +14,6 @@ def collections(request):
 
 def collectionsview(request,slug):
     if (Category.objects.filter(slug=slug, status=0)):
-        print('found')
         products = Product.objects.filter(Category__slug=slug)
         category = Category.objects.filter(slug=slug).first()
         context = {'products':products, 'category': category}
@@ -25,9 +24,7 @@ def collectionsview(request,slug):
     
 def productview(request, cate_slug, prod_slug):
     if(Category.objects.filter(slug=cate_slug, status=0)):
-        print("category found")
         if(Product.objects.filter(slug=prod_slug, status=0)):
-            print("product found")
             products = Product.objects.filter(slug=prod_slug, status=0).first()
             context = {'products':products}
         else:
